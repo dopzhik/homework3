0fe1e00b7e5f9ddc28826f30ca41758715f71d58
Dopzhik <haltura24@gmail.com>
2023-07-02 18:53:25 +0800

    задание 3 выполнено

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая принимает на вход число (N)
+ и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+int ReadInt(string message)
+{
+    System.Console.Write(message);              // Подсказка
+    return Convert.ToInt32(Console.ReadLine()); // Преобразование введенной пользователем строки в число
+}
+void Cube(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        System.Console.Write(i*i*i + ", ");
+    }
+}
+int N = ReadInt("Введите число => ");
+Cube(N);
